02a3b5f3599db3b2721b010a9e4bdb3163198e6e
Vicigel <wzq199001@gmail.com>
2016-04-22 17:03:25 +0800

    add database connection info

--- myblog/settings.py
+++ myblog/settings.py
@@ -77,8 +77,13 @@ WSGI_APPLICATION = 'myblog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django',
+        'USER': 'django',
+        'PASSWORD': 'django',
+        'HOST': '10.186.21.80',
+        'PORT': '3306',
+
     }
 }
 
