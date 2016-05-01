08ffd69bc7454d302dfb6cea89ac189f1b19a212
wzq <wzq199001@gmail.com>
2016-05-01 16:58:51 +0800

    main commit!

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,15 @@
 from django.http import HttpResponse
+from models import Question
+from django.template import loader
 
 
 def index(request):
-    return HttpResponse("Hello,world.You're at the blog index")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('blog/index.html')
+    context = {
+        'latest_question_list': latest_question_list
+    }
+    return HttpResponse(template.render(context, request))
 
 
 def detail(request,question_id):
--- myblog/settings.py
+++ myblog/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -112,7 +113,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'RPC'
 
 USE_I18N = True
 
