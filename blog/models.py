c9dbe71014ae782c77e33a870020a633eac49655
wzq <wzq199001@gmail.com>
2016-05-02 21:40:24 +0800

    main commit!

--- blog/models.py
+++ blog/models.py
@@ -28,3 +28,15 @@ class Choice(models.Model):
     def __str__(self):
         return self.choice_text
 
+
+class BlogCategory(models.Model):
+    category_id = models.SmallIntegerField(null=False)
+    category_name = models.CharField(max_length=50,null=False)
+
+
+class BlogInfo(models.Model):
+    category_id = models.ForeignKey(BlogCategory,related_name="category_id")
+    blog_id = models.IntegerField()
+    create_date = models.DateTimeField(default=timezone.now())
+    author = models.CharField(max_length=50)
+
