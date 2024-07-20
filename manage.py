7703b6527b3b8d843529bba67efedcd5c50e53b3
wangzhanquan <wzq199001@gmail.com>
2024-07-20 21:20:08 +0800

    jewoifjwofo

--- manage.py
+++ manage.py
@@ -8,5 +8,6 @@ if __name__ == "__main__":
     from django.core.management import execute_from_command_line
     # fheiwfowfw
     # new branch
+    # fjeiwoojfwjofw
 
     execute_from_command_line(sys.argv)
